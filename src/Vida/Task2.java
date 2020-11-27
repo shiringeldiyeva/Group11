4887d105e8d337f5c88be36fab6f061134e7beaf
Vida Duodu <vidash50@gmail.com>
2020-11-27 10:55:18 -0500

    added a class and message

---
+++ src/Vida/Task2.java
@@ -0,0 +1,11 @@
+package Vida;
+
+public class Task2 {
+    public static void main(String[] args) {
+
+
+        System.out.println(" working together");
+
+        // this is all we need
+    }
+}

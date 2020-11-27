54dc4d7cb2c8230ac73763b6a6bdf0b949031a8e
Ana Stati <ana.stati18@gmail.com>
2020-11-27 10:49:03 -0500

    more lines added

--- src/Ana/Ana.java
+++ src/Ana/Ana.java
@@ -4,6 +4,9 @@ public class Ana {
     public static void main(String[] args) {
         System.out.println("Hello Group 11");
         //first line
+       //we are doing great
+        //practice day
+
     }
 
 }

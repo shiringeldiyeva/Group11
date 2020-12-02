d783f3f6639dca1f5f1bdc9c29bc34d13c8f0f64
Marwan Subhi <marwan.subhi4@gmail.com>
2020-12-01 21:36:33 -0500

    Added new stuff

---
+++ src/Marwan/Methods/IntQuesRemoveDuplicates.java
@@ -0,0 +1,42 @@
+package Marwan.Methods;
+
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+import sun.security.util.AuthResources_it;
+
+import java.util.Arrays;
+
+public class IntQuesRemoveDuplicates { // remove duplicates == find unique characters
+    public static void main(String[] args) {
+        String str = "AAABBBCCCDEF";
+
+       String str2 = "";
+
+       String [] arr = str.split("");
+
+
+
+//       for (String each1: arr) if (!str2.contains(each1)) str2+=each1;
+//        System.out.println(str2);
+
+
+
+//
+//       for (char each1 : str.toCharArray()) if (!str2.contains(each1+"")) str2+=each1;
+//        System.out.println(str2);
+
+
+       for (int j = 0; j<str.length(); j++){
+           if (!str2.contains(str.charAt(j)+"")){
+               str2+=str.charAt(j);
+           }
+
+       }
+        System.out.println(str2);
+
+
+
+
+
+
+    }
+}

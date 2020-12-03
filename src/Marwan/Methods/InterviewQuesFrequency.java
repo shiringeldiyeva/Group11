5c1c31cb860b3acbcd6467de7bdef504de87cb30
Marwan Subhi <marwan.subhi4@gmail.com>
2020-12-02 20:54:18 -0500

    Added new note

---
+++ src/Marwan/Methods/InterviewQuesFrequency.java
@@ -0,0 +1,41 @@
+package Marwan.Methods;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class InterviewQuesFrequency {
+    public static void main(String[] args) {
+
+        String str = "AABBCCDEF";
+
+        String newString = "";
+
+
+//        ArrayList<Character> list = new ArrayList<>();
+
+//        for (char each : str.toCharArray()){
+//            list.add(each);
+//        }
+//        for (char each :list){
+//           int count = Collections.frequency(list,each);
+//            if (newString.contains(each+""))continue;
+//            newString+=each+""+count;
+//        }
+//        System.out.println(newString);
+
+        ArrayList<String> list = new ArrayList<>();
+
+        //for (String each : str.split(""))list.add(each);
+
+        list.addAll(Arrays.asList(str.split("")));
+        for (String each : list) {
+            int freq = Collections.frequency(list, each);
+            if (!newString.contains(each)) newString += each +freq;
+        }
+        System.out.println(newString);
+
+
+
+    }
+}

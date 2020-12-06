223de22b56bdb48d6e634fffb02cbfa30c6a09ed
Marwan Subhi <marwan.subhi4@gmail.com>
2020-12-05 22:46:00 -0500

    Added Jildiz Package

---
+++ src/Jilidz/Areas.java
@@ -0,0 +1,9 @@
+package Jilidz;
+
+public class Areas {
+    public static void main(String[] args) {
+        double squareSide = 3.5;
+        double areaOfSquare = squareSide*squareSide;
+        System.out.println("area of square is: "+areaOfSquare);
+    }
+}
